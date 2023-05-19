ce36cd94a5a9d93d5128edfd5705ce5a9d010ba3
João Gomes <joaovgsilva20@gmail.com>
2023-05-19 09:53:52 -0300

    modified-HTML

---
+++ js/08_eventos.js
@@ -0,0 +1,21 @@
+/* EVENTOS:
+1. são ações disparadas pela interação dos usuários na página;
+2. o correto manejo desses eventos que tornam as páginas interativas e dinâmicas. 
+
+os eventos mais utilizados:
+
+1. "onclick" = disparado quando recebe um click;
+2. "ondblclick" = disparado quando clique duplo;
+3. "onmouseover" = disparado quando o mouse está sobre;
+4. "onmouseout" = disparado quando o mouse é movido para fora do elemento;
+5. "onmousemove" = disparado quando o mouse é movido no elemento;
+6. "onmousedown" = disparado quando o clique do botão foi pressionado; 
+7. "onmouseup" = disparado quando o clique do botão é liberado;
+8. "onfocus" = disparado qunado o elemento recebe o foco;
+9. "onchange" = disparado quando existe uma mudança no conteúdo ("ao mudar"); 
+10. "onblur" = disparado quando o elemento perde o foco;
+11. "onkeydown" = disparado quando uma tecla é pressionada;
+12. "onkeypress" = disparado quando uma tecla é pressionada e solta;
+13. "onkeyup" = disparado quando uma tecla é solta sobre um elemento;
+14. "onload" = disparado quando a página terminou de ser carregada (body);
+15. "onresize" = disparado quando há um redimencionamneto da janela. */
